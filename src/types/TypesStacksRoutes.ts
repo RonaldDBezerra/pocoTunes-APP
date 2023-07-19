e624041282af7f0f3ce020dc787fe54ec3c0a719
RonaldDBezerra <rebulgame@gmail.com>
2023-07-19 14:52:37 -0300

    Feat: Add news types

--- src/types/TypesStacksRoutes.ts
+++ src/types/TypesStacksRoutes.ts
@@ -4,8 +4,48 @@ type RootStackParamList = {
   Login: undefined; 
   Create: undefined;
   ForgotPassword: undefined;
+  Tab: undefined;
 };
+
+interface IMusicReturn {
+  artistId: string,
+  artistName: string,
+  collectionId: string,
+  collectionName: string,
+  collectionPrice: string,
+  artworkUrl100: string,
+  releaseDate: string,
+  trackCount: string,
+}
+
+type PropsHeading = {
+  title: string
+}
+
+type PropsFeatureAlbuns = {
+  title: string
+}
+
+type PropsRenderItem = {
+  key: number,
+  item: {
+    artistId: string
+    artistName: string
+    collectionId: string
+    collectionName: string
+    collectionPrice: string
+    artworkUrl100: string
+    releaseDate: string
+    trackCount: string
+  }
+}
 
 type PropsStacksScreen = NativeStackScreenProps<RootStackParamList>;
 
-export default PropsStacksScreen
+export {
+  IMusicReturn,
+  PropsStacksScreen,
+  PropsHeading,
+  PropsFeatureAlbuns,
+  PropsRenderItem
+}
